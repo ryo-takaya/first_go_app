ae5b19e8f7d5076eebbb3ca75f910a4bc395291d
ryoutaka <ryoutakaya3623@yahoo.co.jp>
2023-02-06 21:20:42 +0900

    pathを追加

--- main.go
+++ main.go
@@ -1,10 +1,18 @@
 package main
 
 import (
+    "io"
 	"log"
 	"net/http"
 )
 
 func main() {
+	http.HandleFunc("/foo/", func(w http.ResponseWriter, _ *http.Request) {
+                             		io.WriteString(w, "Hello-1\n")
+    })
+	http.HandleFunc("/bar/", func(w http.ResponseWriter, _ *http.Request) {
+                             		io.WriteString(w, "Hello-2\n")
+    })
+
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
