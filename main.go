479b84d31c7174ff61f505a5d7fae388b98f3ff2
ryoutaka <ryoutakaya3623@yahoo.co.jp>
2023-02-12 19:04:03 +0900

    todoをAPIに送信してから画面に送信したtodoを追加する

--- main.go
+++ main.go
@@ -5,9 +5,18 @@ import (
 	"log"
 	"net/http"
 )
+　
+func parseRequest(w http.ResponseWriter, r *http.Request) {
+  e := r.ParseForm()
+  log.Println(e)
+  data := r.Form.Get("data")
+  io.WriteString(w, data)
+}
 
 func main() {
     http.Handle("/", http.FileServer(http.Dir("assets/")))
+
+    http.HandleFunc("/add", parseRequest)
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
