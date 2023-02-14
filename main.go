549a6d98d1618c5264813a3d2af7bb34ce230854
ryoutaka <ryoutakaya3623@yahoo.co.jp>
2023-02-14 12:53:13 +0900

    cookieが設定されていない場合はcookieをセットする

--- main.go
+++ main.go
@@ -1,12 +1,23 @@
 package main
 
 import (
+    "crypto/rand"
+    "math/big"
     "io"
 	"log"
 	"net/http"
 	"fmt"
 	"html/template"
+	"strconv"
 )
+
+func createSessionId() int64 {
+  n, err := rand.Int(rand.Reader, big.NewInt(100000000000000))
+  if err != nil {
+      panic(err)
+  }
+  return n.Int64()
+}
 
 func addAction(w http.ResponseWriter, r *http.Request) {
   e := r.ParseForm()
@@ -19,14 +30,18 @@ func addAction(w http.ResponseWriter, r *http.Request) {
 }
 
 func indexAction(w http.ResponseWriter, r *http.Request) {
+  createSessionId()
   t := template.Must(template.ParseFiles("public/index.html",))
 
-  cookie := &http.Cookie{
-            Name:    "SESID",
-            Value:   "384279583",
-            }
+  cookie, _ := r.Cookie("SESID")
+  if cookie == nil {
+      cookie := &http.Cookie{
+          Name: "SESID",
+          Value: strconv.FormatInt(createSessionId(), 10),
+       }
 
-    http.SetCookie(w, cookie)
+   http.SetCookie(w, cookie)
+  }
     t.Execute(w, nil)
 }
 
